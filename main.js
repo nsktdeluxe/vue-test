6c8509d4fd970f0e9f29550782e57de81a4aab74
nsktdeluxe <tomominishikata@gmail.com>
2021-08-03 13:45:28 +0900

    lesson7 done

--- main.js
+++ main.js
@@ -1,22 +1,24 @@
 var app = new Vue ({
     el: '#app',
     data: {
+        brand: "Vue Mastery",
         product: "Socks",
-        image: "./assets/vmSocks-green.jpeg",
+        selectedVariant: 0,
         altText : "A pair of socks",
         inventory: 100,
-        inStock: false,
         onSale: true,
         details: ["80% Cotton", "20% polyester", "Gender-neutral"],
         variants: [{
             variantId: 2234,
             variantColor: "green",
             variantImage: "./assets/vmSocks-green.jpeg",
+            variantQuantity: 100
         },
         {
             variantId: 2235,
             variantColor: "blue",
             variantImage: "./assets/vmSocks-blue.jpeg",
+            variantQuantity: 0
         }],
         sizes: ["38", "39", "40", "41"],
         cart: 0
@@ -28,8 +30,29 @@ var app = new Vue ({
         removeFromCart() {
             this.cart -= 1
         },
-        UpdateProduct(variantImage){
-            this.image=variantImage
+        UpdateProduct(index){
+            this.selectedVariant=index
+            console.log(index)
+        }
+    },
+    computed: {
+        title() {
+            return this.brand + ' ' + this.product
+        },
+        image() {
+            // 内部では、バリアントの配列である this.variants を返し、
+            // 0または1のいずれかである selectedVariant を使用してその配列の最初または2番目の要素をターゲットにし、
+            // 次にドット表記を使用してその画像をターゲットにしている。
+            return this.variants[this.selectedVariant].variantImage
+        },
+        inStock() {
+            return this.variants[this.selectedVariant].variantQuantity
+        },
+        sale() {
+            if(this.onSale){
+                return this.brand + ' ' + this.product + " " + "are on sale!"
+            }
+                return this.brand + ' ' + this.product  + " " + "are not on sale"
         }
     }
 })
