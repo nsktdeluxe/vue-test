5276ded5da5e76470eb7c10e1cd48a9c5b6e884c
nsktdeluxe <tomominishikata@gmail.com>
2021-08-03 12:34:45 +0900

    lesson6 done

--- main.js
+++ main.js
@@ -5,6 +5,31 @@ var app = new Vue ({
         image: "./assets/vmSocks-green.jpeg",
         altText : "A pair of socks",
         inventory: 100,
-        onSale: true
+        inStock: false,
+        onSale: true,
+        details: ["80% Cotton", "20% polyester", "Gender-neutral"],
+        variants: [{
+            variantId: 2234,
+            variantColor: "green",
+            variantImage: "./assets/vmSocks-green.jpeg",
+        },
+        {
+            variantId: 2235,
+            variantColor: "blue",
+            variantImage: "./assets/vmSocks-blue.jpeg",
+        }],
+        sizes: ["38", "39", "40", "41"],
+        cart: 0
+    },
+    methods: {
+        addToCart(){
+            this.cart +=1
+        },
+        removeFromCart() {
+            this.cart -= 1
+        },
+        UpdateProduct(variantImage){
+            this.image=variantImage
+        }
     }
 })
